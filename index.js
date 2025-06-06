c593f5a4855687be1299d673f4738c4d5900c56e
Legendary S <legendarys3005@gmail.com>
2025-06-06 13:12:51 +0530

    Add/update website files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function downloadInstaller(fileName) {
   const link = document.createElement('a');
   link.href = fileName;  // URL to your installer
-  link.download = 'Rho Installer(0.0.1).exe';  // Suggested file name
+  link.download = 'Rho (0.0.1).exe';  // Suggested file name
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
