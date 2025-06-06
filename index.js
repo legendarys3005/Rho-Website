624413c395120298eda5b2f918652db88625bc1e
Legendary S <legendarys3005@gmail.com>
2025-06-07 00:16:28 +0530

    Add/update website files

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ function downloadInstaller(fileName, downloaded) {
   link.click();
   document.body.removeChild(link);
 }
+
+function animateSvgArrow() {
+  const body = document.getElementById('Body');
+  body.classList.add('slide-up');
+  localStorage.setItem('pageHidden', 'true');
+  const nextSection = document.getElementById("next-page");
+
+  body.classList.add("hidden");
+
+  setTimeout(() => {
+    body.style.display = "none";
+    nextSection.classList.add("show");
+  }, 1000);
+}
+
+// On page load check localStorage
+// window.addEventListener('load', () => {
+// if (localStorage.getItem('pageHidden') === 'true') {
+// const body = document.getElementById('Body');
+// body.classList.add('slide-up');
+// }
+// });
+
+
+window.addEventListener("wheel", (e) => {
+  const bodyDiv = document.getElementById("Body");
+  if (bodyDiv && !bodyDiv.classList.contains("hidden") && e.deltaY > 5) {
+    animateSvgArrow();
+  }
+});
+
+
